d090b361e471702f78fff7c3f42069dde59baf2d
hussein007 <husseinlimem8@gmail.com>
2024-09-26 16:04:12 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var Car = /** @class */ (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    Car.prototype.start = function () {
+        console.log("Car engine started");
+    };
+    return Car;
+}());
+var mycar = new Car("volkswagen", "Golf 7", 2014);
+mycar.start();
